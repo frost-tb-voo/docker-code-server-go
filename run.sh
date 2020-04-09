48b22e7cf0de4ea26a5dc2b91e2f406d70b42784
Novs Yama <frost-tb-voo@users.noreply.github.com>
2020-04-09 23:23:50 +0900

    Add an example golang project.

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ IMAGE=novsyama/code-server-go
 CODER_HOME=/home/coder
 
 S_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-PROJECT_DIR=${S_DIR}/..
+PROJECT_DIR=${S_DIR}/hello
 PROJECT_DIR=`readlink -f ${PROJECT_DIR}`
 
 echo open :8080
-sudo -E docker pull ${IMAGE}
+echo sudo -E docker pull ${IMAGE}
 sudo -E docker stop vscode
 sudo -E docker rm vscode
 sudo -E docker run --name=vscode --net=host -d \
